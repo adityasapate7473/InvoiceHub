d81ea81a92c3cb571ab89f3bff899ac7414cdf22
adityasapate7473 <adityasapate2002@gmail.com>
2025-02-05 15:32:46 +0530

    second commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const port = process.env.PORT || 7000;
 const mongoURI = process.env.MONGO_URI || "mongodb://localhost:27017/invoicehub";
 
 // Connect to MongoDB
-mongoose.connect(mongoURI)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected Successfully"))
     .catch(err => console.error("MongoDB Connection Error:", err));
 
